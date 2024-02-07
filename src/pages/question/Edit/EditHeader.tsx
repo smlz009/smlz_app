0f27a36877c94b7cae530410d8d10079dd4a2cf8
smlz009 <1259826159@qq.com>
2024-02-07 13:42:08 +0800

    feat(EditHeader): ✨ EditHeader

--- src/pages/question/Edit/EditHeader.tsx
+++ src/pages/question/Edit/EditHeader.tsx
@@ -1,11 +1,14 @@
 import React, { FC, useState, ChangeEvent } from 'react'
-import { Button, Typography, Space, Input } from 'antd'
-import { LeftOutlined, EditOutlined } from '@ant-design/icons'
-import { useNavigate } from 'react-router-dom'
+import { Button, Typography, Space, Input, message } from 'antd'
+import { LeftOutlined, EditOutlined, LoadingOutlined } from '@ant-design/icons'
+import { useNavigate, useParams } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
+import { useRequest, useKeyPress, useDebounceEffect } from 'ahooks'
 import useGetPageInfo from '../../../hooks/useGetPageInfo'
+import useGetComponentInfo from '../../../hooks/useGetComponentInfo'
 import { PageInfoType, changePageTitle } from '../../../store/pageInfoReduce'
 import EditToolbar from './EditToolbar'
+import { updateQuestionService } from '../../../services/question'
 import styles from './EditHeader.module.scss'
 
 const { Title } = Typography
@@ -48,7 +51,68 @@ const TitleElem: FC = () => {
 }
 
 const SaveButton: FC = () => {
-  return <Button>保存</Button>
+  const { id } = useParams()
+  const pageInfo = useGetPageInfo() as PageInfoType
+  const { componentList = [] } = useGetComponentInfo()
+
+  const { loading, run: save } = useRequest(
+    async () => {
+      if (!id) return
+      await updateQuestionService(parseInt(id), { ...pageInfo, componentList })
+    },
+    {
+      manual: true
+    }
+  )
+
+  useKeyPress(['ctrl.s', 'meta.s'], (e: KeyboardEvent) => {
+    e.preventDefault() // 阻止默认事件
+    if (!loading) save()
+  })
+
+  //自动保存
+  useDebounceEffect(
+    () => {
+      save()
+    },
+    [componentList, pageInfo],
+    {
+      wait: 1000
+    }
+  )
+
+  return (
+    <Button onClick={save} disabled={loading} icon={loading ? <LoadingOutlined /> : null}>
+      保存
+    </Button>
+  )
+}
+
+const PublishButton: FC = () => {
+  const { id } = useParams()
+  const pageInfo = useGetPageInfo() as PageInfoType
+  const { componentList = [] } = useGetComponentInfo()
+  const nav = useNavigate()
+
+  const { loading, run: pub } = useRequest(
+    async () => {
+      if (!id) return
+      await updateQuestionService(parseInt(id), { ...pageInfo, componentList, isPublished: true })
+    },
+    {
+      manual: true,
+      onSuccess() {
+        message.success('发布成功')
+        nav('/question/stat/' + id)
+      }
+    }
+  )
+
+  return (
+    <Button type="primary" onClick={pub} disabled={loading}>
+      发布
+    </Button>
+  )
 }
 
 const EditHeader: FC = () => {
@@ -71,7 +135,7 @@ const EditHeader: FC = () => {
         <div className={styles.right}>
           <Space>
             <SaveButton />
-            <Button type="primary">发布</Button>
+            <PublishButton />
           </Space>
         </div>
       </div>
